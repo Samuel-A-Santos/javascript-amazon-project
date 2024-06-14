cf3e1dfebd71bb1d53a5c40f250d70659250ded2
Samuel Alessandro Lima dos Santos <samuel@DESKTOP-7FDCQPP>
2024-06-13 23:30:28 -0300

    make delete button interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -100,7 +100,9 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
 
-    document.querySelector(`.js-cart-item-container-${productId}`);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
     container.remove();
   });
 });
