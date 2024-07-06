81b3a79db8ae3618b0055c82686c32795b6794d1
Samuel Alessandro Lima dos Santos <samuel@DESKTOP-7FDCQPP>
2024-07-05 22:01:36 -0300

    intro to backend and API calls

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary";
 import { renderPaymentSummary } from "./checkout/paymentSummary";
 import "../data/cart-class.js";
+import "../data/backend-practice.js";
 
 renderOrderSummary();
 renderPaymentSummary();
