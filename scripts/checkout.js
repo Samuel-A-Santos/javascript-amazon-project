ed9bf7fbb1e9566b128eff3abddeac4416d7eecc
Samuel Alessandro Lima dos Santos <samuel@DESKTOP-7FDCQPP>
2024-07-14 20:17:36 -0300

    load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import { renderOrderSummary } from "./checkout/orderSummary";
 import { renderPaymentSummary } from "./checkout/paymentSummary";
-import "../data/cart-class.js";
-import "../data/backend-practice.js";
+import { loadProducts } from "../data/products";
+// import "../data/cart-class.js";
+// import "../data/backend-practice.js";
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
